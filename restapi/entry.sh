817dc8c62ac6fd877692cbce0bae427d800376fe
Eshaq Farrokhi <71772445+Eshaq-F@users.noreply.github.com>
2023-01-28 19:18:45 +0330

    Update entry.sh

--- restapi/entry.sh
+++ restapi/entry.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-cmd=$1
+var=$1
 
-if [ $cmd = 'RUN' ]; then
-  python manage.py migrate
-  python manage.py seed
-  exec python manage.py runserver 0.0.0.0:8000
+if [ $var = "RUN" ]; then
+    python manage.py migrate
+    python manage.py seed
+    exec python manage.py runserver 0.0.0.0:8000
 fi
